233a7e650cee2da49b061160f9a21c3be59a9d95
shafu <selfouly@gmail.com>
2023-09-13 18:32:21 +0200

    first example

--- src/lexer.rs
+++ src/lexer.rs
@@ -5,28 +5,29 @@ pub struct Lexer {
     path: String,
 }
 
-fn parse_opcode(line: String) {
+fn parse_opcode(stack: &mut Vec<&str>, line: String) {
     let l = line.trim_start().trim_end();
 
     if l.starts_with("0x") {
-        println!("{}", l);
+        stack.push("0x");
     }
-    
+
     if l == "calldataload" {
-        println!("{}", l);
+        stack.pop();
+        stack.push("calldata");
     }
 
     if l.starts_with("[") {
-        println!("{}", l);
+        stack.push("[VALUE_LOCATION]");
     }
 
     if l == "sstore" {
-        println!("{}", l);
+        stack.pop();
+        stack.pop();
     }
-
 }
 
-fn parse_macro(contents: String) -> String{
+fn parse_macro(contents: String) -> String {
     let mut macro_contents = String::new();
 
     let mut in_macro = false;
@@ -67,9 +68,11 @@ impl Lexer {
         let macro_contents = parse_macro(contents.clone());
         // println!("{}", macro_contents);
 
-        for l in macro_contents.lines() {
-            parse_opcode(l.to_string());
-        }
+        let mut stack = Vec::new();
 
+        for l in macro_contents.lines() {
+            parse_opcode(&mut stack, l.to_string());
+            println!("{:?}", stack);
+        }
     }
 }
