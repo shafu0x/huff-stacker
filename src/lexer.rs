fcbe57d52baf2759d7335a3eea55345a9d0462ea
shafu <selfouly@gmail.com>
2023-09-13 18:14:58 +0200

    refactor

--- src/lexer.rs
+++ src/lexer.rs
@@ -5,37 +5,45 @@ pub struct Lexer {
     path: String,
 }
 
+fn lex_macro(contents: String) {
+    let mut macro_contents = String::new();
+
+    let mut in_macro = false;
+    for l in contents.lines() {
+
+        // in macro
+        if in_macro && !l.starts_with("}") {
+            // add line to macro_contents with a new line
+            macro_contents.push_str(l);
+            macro_contents.push_str("\n");
+        }
+        
+        // start of macro
+        if !in_macro && l.starts_with("#define macro") {
+            in_macro = true;
+        }
+
+        // end of macro
+        if in_macro && l.starts_with("}") {
+            in_macro = false;
+        }
+    }
+
+    println!("{}", macro_contents);
+}
+
+
 impl Lexer {
     pub fn new(path: String) -> Lexer {
         Lexer { path: path }
     }
 
-    pub fn read_file(&self) -> String {
+    pub fn read_file(&self) {
         let mut file = File::open(self.path.as_str()).expect("File not found");
         let mut contents = String::new();
         file.read_to_string(&mut contents)
             .expect("Error reading file");
 
-        let mut in_macro = false;
-        for l in contents.lines() {
-
-            // in macro
-            if in_macro && !l.starts_with("}") {
-                println!("In {}", l);
-            }
-            
-            // start of macro
-            if !in_macro && l.starts_with("#define macro") {
-                println!("Start {}", l);
-                in_macro = true;
-            }
-
-            // end of macro
-            if in_macro && l.starts_with("}") {
-                println!("End {}", l);
-                in_macro = false;
-            }
-        }
-        contents
+        lex_macro(contents);
     }
 }
