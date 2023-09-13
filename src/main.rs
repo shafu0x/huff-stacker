79e3eb0d614a2fe2db41bcf2d6c266ddb48f7b22
shafu.eth <selfouly@gmail.com>
2023-09-13 22:46:24 +0200

    read path

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@ mod lexer;
 mod printer;
 mod stack;
 
+use std::env;
 use lexer::Lexer;
 
-const PATH: &str = "/home/shafu/huff-stack-generator/macro.txt";
-
 fn main() {
-    let lexer = Lexer::new(PATH.to_string());
+    let args: Vec<String> = env::args().collect();
+    let lexer = Lexer::new(args[1].to_string());
     lexer.read_file();
 }
