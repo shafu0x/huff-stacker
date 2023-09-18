39fc2453aeead79f879aee306b35feb1582f4d23
shafu <selfouly@gmail.com>
2023-09-18 19:34:31 +0200

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,11 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let path_in = args[1].to_string();
-    let path_out = args[2].to_string();
+    //let path_in = args[1].to_string();
+    //let path_out = args[2].to_string();
+
+    let path_in = "/home/shafu/huff-stack-generator/macro.huff";
+    let path_out = "/home/shafu/huff-stack-generator/out.huff";
 
     let functions = parse(&path_in);
     write(&path_in, &functions, &path_out);
