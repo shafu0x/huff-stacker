572d0b71e9398d5e06e710229b31fe00320d59c0
shafu <selfouly@gmail.com>
2023-09-20 18:19:45 +0200

    bash noob

--- run.sh
+++ run.sh
@@ -3,10 +3,4 @@
 PATH_IN=$HOME/huff-stacker/macro.huff
 PATH_OUT=$HOME/huff-stacker/out.huff
 
-cargo run $PATH_IN $PATH_OUT
-
-if [ $? -eq 0 ]; then
-  cat out.huff
-else
-  echo "Failure"
-fi
+cargo run $PATH_IN $PATH_OUT && cat out.huff
